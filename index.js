6229feeb047ca8168924cec4e569ce1ff44e50ff
Monalisa <monihank@gmail.com>
2022-06-01 19:29:34 -0300

    second commit

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ app.post('/', function (req, res) {
 
 
 //creating the server instance
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server has started on port 3000.");
 });
